825e4c3fa37bc2a4b5ee7e83910c2121b1d4f81d
Mark Franco <marfra@microsoft.com>
2022-02-20 13:24:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Identity.Client;
+
+string[] scopes = new string[] { "api://***REMOVED***/Weather.Read" };
+var app = PublicClientApplicationBuilder.Create("***REMOVED***")
+        .WithTenantId("***REMOVED***")
+        .WithDefaultRedirectUri()
+        .Build();
+var accounts = await app.GetAccountsAsync();
+AuthenticationResult result;
+try
+{
+    result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
+                .ExecuteAsync();
+}
+catch (MsalUiRequiredException)
+{
+    result = await app.AcquireTokenInteractive(scopes)
+                .ExecuteAsync();
+}
+
+Console.WriteLine(result.AccessToken);
+
+using (HttpClient httpClient = new HttpClient())
+{
+    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", result.AccessToken);
+
+    var res = httpClient.GetAsync("https://localhost:7104/WeatherForecast").Result;
+
+}
