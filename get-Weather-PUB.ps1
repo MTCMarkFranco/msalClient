ef2db83ab0e42aa2535c25e11bd813cacad7966c
Mark Franco <marfra@microsoft.com>
2022-02-20 13:26:05 -0500

    Added PSHell

---
+++ get-Weather-PUB.ps1
@@ -0,0 +1,16 @@
+﻿#Install-Module MSAL.PS -SkipPublisherCheck -Force
+#Install-Module -name MSAL.PS -Force -AcceptLicense
+
+
+$TenantId = "***REMOVED***"  # aka Directory ID. This value is Microsoft tenant ID
+$ClientId = "***REMOVED***"  # aka Application ID
+
+$token = Get-MsalToken -ClientId $ClientId -Scope 'api://***REMOVED***/Weather.Read' -TenantId $TenantId -RedirectUri "http://localhost"
+
+$Auth =  "Bearer " + $token.AccessToken
+
+
+$result = curl -Method Get 'https://localhost:7104/WeatherForecast' -H @{ "accept" = "text/plain"; "Authorization" = $Auth}
+
+
+$result
