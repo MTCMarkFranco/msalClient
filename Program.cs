e7b056c17a686939808cf26fdab60324d562d124
Mark Franco <marfra@microsoft.com>
2023-01-10 20:10:16 -0500

    Working Code, DO NOT PUSH TO ORIGIN

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
-﻿using Microsoft.Identity.Client;
+﻿using System.Text.Json;
+using Microsoft.Identity.Client;
+using callRecords.Models;
 
 // Initialize
-AuthenticationResult result;
+AuthenticationResult? result = null;
 
-// Get Token using MSAL with the proper Audience and Scope and using the Public Client Application in MSAL with Interactive Login (accommodates MFA and Conditional Access Policies)
-string[] scopes = new string[] { "api://<CLIENT_ID_OR_APP_FQDN>/Weather.Read" };
-var app = PublicClientApplicationBuilder.Create("<CLIENT_ID_OF_NATIVE_APP_REG>")
-        .WithTenantId("<TENANT_ID_OF_APP_REG>")
-        .WithDefaultRedirectUri()
-        .Build();
-
-var accounts = await app.GetAccountsAsync();
+string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+var app = ConfidentialClientApplicationBuilder.Create("f976c105-c8e4-4635-8ddf-e8acab8c7345")
+                                          .WithClientSecret("***REMOVED***")
+                                          .WithAuthority(new Uri("https://login.microsoftonline.com/b8ba91e8-4402-47de-9d97-12a0faaa0115"))
+                                          .Build();
 
 try
 {
-    result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
-                .ExecuteAsync();
+    result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
 }
 catch (MsalUiRequiredException)
 {
-    result = await app.AcquireTokenInteractive(scopes)
-                .ExecuteAsync();
+    
 }
 
 // DEBUG
 Console.WriteLine(result.AccessToken);
 
-// Call our Protected Web API
 using (HttpClient httpClient = new HttpClient())
 {
     httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", result.AccessToken);
 
-    var res = httpClient.GetAsync("https://localhost:7104/WeatherForecast").Result;
+    var res = httpClient.GetAsync("https://graph.microsoft.com/v1.0/communications/callRecords/getPstnCalls(fromDateTime=2023-01-09,toDateTime=2023-01-11)").Result;
+
+    Console.WriteLine(res.Content.ReadAsStringAsync().Result.ToString());
+
+    var callLogRows = JsonSerializer.Deserialize<pstnCallLogRow>(res.Content.ReadAsStringAsync().Result.ToString());
 
 }
